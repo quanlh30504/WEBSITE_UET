11552856d3a33e15d6895a059b92d127bb355508
Quan Nguyen <quanlh3052004@gmail.com>
2023-09-28 00:40:57 +0700

    update manager by Quan Nguyen

---
+++ admin/partials/menu.php
@@ -0,0 +1,24 @@
+
+<!-- Maded by Quan Nguyen on 27/9/2023 -->
+
+<html>
+
+<head>
+    <title>Food order Website - Home Page</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+    <!-----Menu section Starts----->
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="manage-admin.php">Admin</a></li>
+                <li><a href="manage-category.php">Category</a></li>
+                <li><a href="manage-food.php">Footer</a></li>
+                <li><a href="manage-order.php">Order</a></li>
+            </ul>
+        </div>
+    </div>
+    <!---- Menu Section Ends------->
