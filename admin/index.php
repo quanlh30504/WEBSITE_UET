8a70116ad95866a3913e8d98ec63177f4079cf00
Quan Nguyen <quanlh3052004@gmail.com>
2023-09-27 23:47:06 +0700

    update front end v1 - by Quan Nguyen

---
+++ admin/index.php
@@ -0,0 +1,61 @@
+<html>
+    <head>
+        <title>Food order Website - Home Page</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+
+    <body>
+        <!-----Menu section Starts----->
+        <div class="menu text-center">
+            <div class="wrapper">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Admin</a></li>
+                    <li><a href="#">Category</a></li>
+                    <li><a href="#">Footer</a></li>
+                    <li><a href="#">Order</a></li>
+                </ul> 
+            </div>              
+        </div>
+        <!---- Menu Section Ends------->
+        
+
+        <!-----Main Content Section Starts-->
+        <div class="main-content">
+            <div class="wrapper">
+                <h1>Dashboard</h1>
+                <div class="col-4 text-center">
+                    <h1>5</h1>
+                    <br />
+                    CATOGORY
+                </div>
+
+                <div class="col-4 text-center">
+                    <h1>5</h1>
+                    <br />
+                    CATOGORY
+                </div>
+
+                <div class="col-4 text-center">
+                    <h1>5</h1>
+                    <br />
+                    CATOGORY
+                </div>
+
+                <div class="col-4 text-center">
+                    <h1>5</h1>
+                    <br />
+                    CATOGORY
+                </div>
+
+                <div class="clearfix"></div>
+            </div>   
+        </div>
+        <!-----Footer Section Starts------>
+        <div class="footer">
+            <div class="wrapper">
+                    <p class="text-center">2023 All right reversed, Some Resterant. Developed By - <a href="https://www.facebook.com/profile.php?id=100020028467065">Quan Nguyen</a></p>       
+            </div>
+        <!-------footer section end------->  
+    </body>
+</html>
